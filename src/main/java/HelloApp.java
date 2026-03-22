a0c015875bcaf5e5d37b276baa7733962bec6360
shrivallipahade02-spec <shrivallipahade02@gmail.com>
2026-03-22 11:49:19 +0530

    UC4: Display multiple names from command-line arguments

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,7 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         } else {
             System.out.println("Hello World");
         }
